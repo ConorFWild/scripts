a52e71478b23162060f11c6d7eaa1cac1fe7c0f7
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-10-27 11:37:21 +0000

    t

--- test_pandda.sh
+++ test_pandda.sh
@@ -1,8 +1,9 @@
-pandda="/data/share-2/conor/code/pandda_gemmi/pandda_gemmi/analyse.py"
-data_dirs="/data/share-2/conor/pandda_inputs/PHIPA"
+pandda="/data/share-2/conor/pandda/pandda_gemmi/pandda_gemmi/analyse.py"
+data_dirs="/data/share-2/conor/pandda/data/pandda_inputs/PHIPA"
 cluster_cutoff_distance_multiplier=1.1 
 min_blob_volume=9.0
-out_dir="/data/share-2/conor/scratch/test_pandda"
+out_dir="/data/share-2/conor/pandda/output/test/test_pandda"
+
 
 python $pandda --data_dirs=$data_dirs --cluster_cutoff_distance_multiplier=$cluster_cutoff_distance_multiplier --min_blob_volume=$min_blob_volume --out_dir=$out_dir
 
